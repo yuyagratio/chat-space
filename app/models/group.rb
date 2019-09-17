a7201f3651004d037e56dc1780e3c18eb9052b78
terakkyo <yuya.soccerlove17@gmail.com>
2019-09-17 14:41:30 +0900

    edit-message-send-function

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,7 @@
 class Group < ApplicationRecord
   has_many :group_users
   has_many :users, through: :group_users
+  has_many :messages
+  
   validates :name, presence: true, uniqueness: true
 end
