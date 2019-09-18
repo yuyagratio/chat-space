91a2b56ae22d7549fb1f78c3a785503ac17547cc
terakkyo <yuya.soccerlove17@gmail.com>
2019-09-18 19:14:53 +0900

    Update messages_controller_spec.rb
    messagesコントローラでメッセージを作成するアクションのテストを行うコードの記述をした。
    
    実際に書いたテストコードは以下のことが正しく実行されているか確認している。
    
    ・ログインしているかつ、保存に成功した場合
    メッセージの保存はできたのか
    意図した画面に遷移しているか
    
    ・ログインしているが、保存に失敗した場合
    メッセージの保存は行われなかったか
    意図したビューが描画されているか
    
    ・ログインしていない場合
    意図した画面にリダイレクトできているか

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -35,4 +35,57 @@ describe MessagesController do
       end
     end
   end
+
+  describe '#create' do
+    let(:params) { { group_id: group.id, user_id: user.id, message: attributes_for(:message) } }
+
+    context 'log in' do
+
+      before do
+        login user
+      end
+
+      context 'can save' do
+        subject {
+          post :create,
+          params: params
+        }
+
+        it 'count up message' do
+          expect{ subject }.to change(Message, :count).by(1)
+        end
+
+        it 'redirects to group_messages_path' do
+          subject
+          expect(response).to redirect_to(group_messages_path(group))
+        end
+      end
+      
+      context 'can not save' do
+        let(:invalid_params) { { group_id: group.id, user_id: user.id, message: attributes_for(:message, content: nil, image: nil) } }
+
+        subject {
+          post :create,
+          params: invalid_params
+        }
+
+        it 'dose not count up' do
+          expect{ subject }.not_to change(Message, :count)
+        end
+
+        it 'renders index' do
+          subject
+          expect(response).to render_template :index
+        end
+      end
+    end
+
+    context 'not log in' do
+
+      it 'redirects to new_user_session_path' do
+        post :create, params: params
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
 end
