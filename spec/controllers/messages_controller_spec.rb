81ba1ce3ee136f7ed43b2139f7491c34a1e4ac33
terakkyo <yuya.soccerlove17@gmail.com>
2019-09-18 17:53:35 +0900

    add-messages_controller_spec.rb
    messagesコントローラでメッセージ一覧を表示するアクションのテストを行う
    
    内容としては、
    メッセージ一覧ページを表示するアクションで
    ・ログインしている場合
    アクション内で定義しているインスタンス変数があるか
    該当するビューが描画されているか
    ・ログインしていない場合
    意図したビューにリダイレクトできているか
    
    上記のパターンをテストするコードを記述した

---
+++ spec/controllers/messages_controller_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+describe MessagesController do
+  let(:group) { create(:group) }
+  let(:user) { create(:user) }
+
+  describe '#index' do
+
+    context 'log in' do
+      before do
+        login user
+        get :index, params: { group_id: group.id}
+      end
+
+      it 'assigns @message' do
+        expect(assigns(:message)).to be_a_new(Message)
+      end
+
+      it 'assigns @group' do
+        expect(assigns(:group)).to eq group
+      end
+
+      it 'renders index' do
+        expect(response).to render_template :index
+      end
+    end
+
+    context 'not log in' do
+      before do
+        get :index, params: { group_id: group.id }
+      end
+
+      it 'redirects to new_user_session_path' do
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+end
