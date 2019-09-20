fa46f507550871330854de207f5dd5a083ee0903
Terakkyo <46860363+SnakeY9@users.noreply.github.com>
2019-09-20 17:23:57 +0900

    Revert "メッセージ送信機能に非同期通信機能の追加"

--- Gemfile
+++ Gemfile
@@ -17,7 +17,6 @@ gem 'coffee-rails', '~> 4.2'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
-gem 'jquery-ui-rails'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
 gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
@@ -57,4 +56,5 @@ gem 'font-awesome-rails'
 gem 'devise'
 gem 'carrierwave'
 gem 'mini_magick'
+gem 'jquery-rails'
 gem 'rails-ujs'
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,14 +9,11 @@ class MessagesController < ApplicationController
   def create
     @message = @group.messages.new(message_params)
     if @message.save
-      respond_to do |format|
-        format.html {redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'}
-        format.json
-      end
+      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
     else
-        @messages = @group.messages.includes(:user)
-        flash.now[:alert] = 'メッセージを入力してください。'
-        render :index
+      @messages = @group.messages.includes(:user)
+      flash.now[:alert] = 'メッセージを入力してください。'
+      render :index
     end
   end
 
