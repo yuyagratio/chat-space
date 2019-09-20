240d664ea556ce4755fc879b51f98343feabed43
terakkyo <yuya.soccerlove17@gmail.com>
2019-09-20 13:54:34 +0900

    respond_toを使用してHTMLとJSONの場合で処理わけ
    messages#createでメッセージを保存し、respond_toを使用してHTMLとJSONの場合で処理を分ける

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function() {
   $("#new_message").on("submit", function(e) {
     e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).attr('action');j
+    var url = $(this).attr('action');
     $.ajax({
       url: url,
       type: "POST",
