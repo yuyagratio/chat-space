5a2de573b1c0a5d46eb34a14fe93ea497ea4f088
terakkyo <yuya.soccerlove17@gmail.com>
2019-09-21 16:14:50 +0900

    Update user_search.js
    グループ作成時にグループに追加するユーザーを検索する欄で、イベントが発生した際に、非同期通信でユーザーを検索できるように記述した

--- app/assets/javascripts/user_search.js
+++ app/assets/javascripts/user_search.js
@@ -1,7 +1,15 @@
 $(function() {
+
   $(function() {
     $("#user-search-field").on("keyup", function() {
       var input = $("#user-search-field").val();
+
+      $.ajax({
+        url: "/users",
+        type: "GET", 
+        data: {keyword: input},
+        dataType: "json"
+      })
     })
   });
 });
