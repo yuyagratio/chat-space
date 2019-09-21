9d93a7c4f74e83272c8a7fe246d8cfde69ac5c82
terakkyo <yuya.soccerlove17@gmail.com>
2019-09-21 15:00:52 +0900

    Update users_controller.rb
    ユーザーを取得するコードを前回に書いたので今回はjbuilderを使ってJavaScript側に返す処理を記述した。
    複数のユーザーが格納された配列を返す記述を行った

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
 
   def index
     @users = User.where("name LIKE(?)", "%#{params[:keyword]}%").where.not(id: current_user)
+    respond_to do |format|
+      format.json
+    end
   end
 
   def edit
