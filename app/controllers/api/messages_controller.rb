730a4a51b309ec06cacf5fae28d1ef716c9ba86d
terakkyo <yuya.soccerlove17@gmail.com>
2019-09-22 21:04:24 +0900

    Create controller/api/messages_controller.rb
    メッセージを更新するためのアクションを実装

---
+++ app/controllers/api/messages_controller.rb
@@ -0,0 +1,10 @@
+class API::MessagesController < ApplicationController
+  def index
+    # ルーティングでの設定によりparamsの中にgroup_idというキーでグループのidが入るので、これを元にDBからグループを取得する
+    group = Group.find(params[:group_id])
+    # ajaxで送られてくる最後のメッセージのid番号を変数に代入
+    last_message_id = params[:id].to_i
+    # 取得したグループでのメッセージ達から、idがlast_message_idよりも新しい(大きい)メッセージ達のみを取得
+    @messages = group.messages.includes(:user).where("id > #{last_message_id}")
+  end
+end
