d93e908771f755fdb308bd50bf1b9af0d456e683
terakkyo <yuya.soccerlove17@gmail.com>
2019-09-23 18:58:58 +0900

    Update message.js
    空白消去

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,7 @@
-$(function() {
-  function buildHTML(message){
+$(document).on("turbolinks:load", function() {
+  function buildHTML(message) {
     var image = message.image ? `<img src"=${message.image}">` : "";
-    var html = `<div class="message" data-id=${message.id}>
+    var html = `<div class="message" data-message-id=${message.id}>
                   <div class="message__upper-info">
                     <div class="message__upper-info__user">
                       ${message.user_name}
@@ -23,12 +23,12 @@ $(function() {
   $("#new_message").on("submit", function(e) {
     e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).attr('action');
+    var url = $(this).attr("action");
     $.ajax({
       url: url,
       type: "POST",
       data: formData,
-      dataType: 'json',
+      dataType: "json",
       processData: false,
       contentType: false
     })
@@ -37,33 +37,42 @@ $(function() {
       $(".messages").append(html);
       $(".new-message__submit-btn").attr("disabled", false);
       $("#new_message")[0].reset();
-      $(".messages").animate({ scrollTop: $(".messages")[0].scrollHeight }, "first");
+      $(".messages").animate(
+        { scrollTop: $(".messages")[0].scrollHeight },
+        "first"
+      );
     })
     .fail(function() {
-      alert('メッセージを入力してください');
+      alert("メッセージを入力してください");
     });
-    return false;
   });
 
   var reloadMessages = function() {
-    last_message_id = $(".message:last").data("id");
-    $.ajax({
-      url: "api/messages",
-      type: 'get',
-      dataType: 'json',
-      data: {id: last_message_id}
-    })
-    .done(function(messages){
-      var insertHTML = '';
-      messages.forEach(function(message){
-        insertHTML += buildHTML(message);
-        $(".messages").append(insertHTML);
+    if (window.location.href.match(/\/groups\/\d+\/messages/)) {
+      // last_message_idはこの記述で値が入っていることをconsole.logで確認済み
+      var last_message_id = $(".message:last").data("message-id");
+      console.log(last_message_id);
+      $.ajax({
+        url: "api/messages",
+        type: "GET",
+        dataType: "json",
+        data: {id: last_message_id }
+      })
+      .done(function(messages) {
+        var insertHTML = "";
+        messages.forEach(function(message) {
+          insertHTML += buildHTML(message);
+          $(".messages").append(insertHTML);
+        });
+        $(".messages").animate(
+          { scrollTop: $(".messages")[0].scrollHeight },
+          "first"
+        );
+      })
+      .fail(function() {
+        console.log("error");
       });
-      $(".messages").animate({ scrolsslTop: $(".messages")[0].scrollHeight }, "first");
-    })
-    .fail(function(){
-      console.log('error');
-    });
+    }
   };
   setInterval(reloadMessages, 5000);
 });
