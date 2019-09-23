51327bf10817c2689d12cf27a7e2fcc8a3c8ad3e
terakkyo <yuya.soccerlove17@gmail.com>
2019-09-23 09:49:48 +0900

    Update message.js
    追加したアクションをリクエストするように実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -44,4 +44,20 @@ $(function() {
     });
     return false;
   });
+
+  var reloadMessages = function() {
+    last_message_id = $(".message:last").data("id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages){
+      console.log('success');
+    })
+    .fail(function(){
+      console.log('error');
+    });
+  };
 });
