711da6d2d6593936b2774469fd3d88073f8a8dde
terakkyo <yuya.soccerlove17@gmail.com>
2019-09-23 19:23:53 +0900

    Update users_controller.rb
    現在ログイン中のユーザーのidを指定したいのに、してなかったので指定するようにコードを修正
    
    editアクションに不要な記述があったので修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,13 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.where("name LIKE(?)", "%#{params[:keyword]}%").where.not(id: current_user)
+    @users = User.where("name LIKE(?)", "%#{params[:keyword]}%").where.not(id: current_user.id)
     respond_to do |format|
       format.json
     end
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
