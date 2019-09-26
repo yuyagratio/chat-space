d3f9ba034f204a6c4a95661c66e1c5b9424e3aed
terakkyo <yuya.soccerlove17@gmail.com>
2019-09-26 21:14:40 +0900

    commit1

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,8 @@ set :linked_files, %w{ config/secrets.yml }
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
-    invoke 'unicorn:restart'
+    invoke 'unicorn:stop'
+    invoke 'unicorn:start'
   end
 
   desc 'upload secrets.yml'
