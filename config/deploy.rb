6b4ff4411739e37a270a399e2aead556ac4f9719
terakkyo <yuya.soccerlove17@gmail.com>
2019-09-27 11:47:55 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,8 +21,8 @@ set :keep_releases, 5
 set :default_env, {
   rbenv_root: "/usr/local/rbenv",
   path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
-  # AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
-  # AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
 }
 
 set :linked_files, %w{ config/secrets.yml }
