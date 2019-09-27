1a4eb0ceb35c9bd8f5e9246d752554e1fbf50cc5
terakkyo <yuya.soccerlove17@gmail.com>
2019-09-27 16:01:53 +0900

    Update users_controller.rb
    あいまい検索になっていたので、検索ワードが最初にヒットしないとインクリメンタルサーチが起動しないように変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.where("name LIKE(?)", "%#{params[:keyword]}%").where.not(id: current_user.id)
+    @users = User.where("name LIKE(?)", "#{params[:keyword]}%").where.not(id: current_user.id)
     respond_to do |format|
       format.json
     end
